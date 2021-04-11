7467d01f8b907b3c5b76601085ab1b7abfa12c21
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-11 02:43:53 -0400

    Try1

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ import urllib.request
 from pathlib import Path
 from typing import List, NamedTuple
 
-#camera.py packages
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
-import imutils
-import cv2, os, urllib.request
-import numpy as np
-from django.conf import settings
-import os
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -23,11 +14,6 @@ try:
 except ImportError:
     from typing_extensions import Literal  # type: ignore
 
-import pyav
-import cv2
-import numpy as np
-import streamlit as st
-from aiortc.contrib.media import MediaPlayer
 
 
 
@@ -143,112 +129,13 @@ def main():
     elif app_mode == loopback_page:
         app_loopback()
 
-def app_mask_Detect():
-   class MaskDetect(object):
-    def __init__(self):
-        self.vs = VideoStream(src=0).start()
 
-    def __del__(self):
-        cv2.destroyAllWindows()
 
-    def detect_and_predict_mask(self, frame, faceNet, maskNet):
-        # grab the dimensions of the frame and then construct a blob
-        # from it
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-                                     (104.0, 177.0, 123.0))
 
-        # pass the blob through the network and obtain the face detections
-        faceNet.setInput(blob)
-        detections = faceNet.forward()
 
-        # initialize our list of faces, their corresponding locations,
-        # and the list of predictions from our face mask network
-        faces = []
-        locs = []
-        preds = []
 
-        # loop over the detections
-        for i in range(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the detection
-            confidence = detections[0, 0, i, 2]
 
-            # filter out weak detections by ensuring the confidence is
-            # greater than the minimum confidence
-            if confidence > 0.5:
-                # compute the (x, y)-coordinates of the bounding box for
-                # the object
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
 
-                # ensure the bounding boxes fall within the dimensions of
-                # the frame
-                (startX, startY) = (max(0, startX), max(0, startY))
-                (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-                # extract the face ROI, convert it from BGR to RGB channel
-                # ordering, resize it to 224x224, and preprocess it
-                face = frame[startY:endY, startX:endX]
-                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                face = cv2.resize(face, (224, 224))
-                face = img_to_array(face)
-                face = preprocess_input(face)
-
-                # add the face and bounding boxes to their respective
-                # lists
-                faces.append(face)
-                locs.append((startX, startY, endX, endY))
-
-        # only make a predictions if at least one face was detected
-        if len(faces) > 0:
-            # for faster inference we'll make batch predictions on *all*
-            # faces at the same time rather than one-by-one predictions
-            # in the above `for` loop
-            faces = np.array(faces, dtype="float32")
-            preds = maskNet.predict(faces, batch_size=32)
-
-        # return a 2-tuple of the face locations and their corresponding
-        # locations
-        return (locs, preds)
-
-    def get_frame(self):
-         frame = self.vs.read()
-         frame = imutils.resize(frame, width=650)
-         frame = cv2.flip(frame, 1)
-         # detect faces in the frame and determine if they are wearing a
-          #face mask or not
-         (locs, preds) = self.detect_and_predict_mask(frame, faceNet, maskNet)
-
-        # loop over the detected face locations and their corresponding
-        # locations
-         for (box, pred) in zip(locs, preds):
-            # unpack the bounding box and predictions
-            (startX, startY, endX, endY) = box
-            (mask, withoutMask) = pred
-
-            # determine the class label and color we'll use to draw
-            # the bounding box and text
-            label = "Mask" if mask > withoutMask else "No Mask"
-            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-            # include the probability in the label
-            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-            # display the label and bounding box rectangle on the output
-            # frame
-            cv2.putText(frame, label, (startX, startY - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-         ret, jpeg = cv2.imencode('.jpg', frame)
-         return jpeg.tobytes()
-   webrtc_ctx = webrtc_streamer(
-        key="mask-detect",
-        mode=WebRtcMode.SENDRECV,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-        video_transformer_factory=MaskDetect,
-        async_transform=True,
-    )
 
 def app_loopback():
     """ Simple video loopback """
@@ -259,6 +146,140 @@ def app_loopback():
         video_transformer_factory=None,  # NoOp
     )
 
+
+
+def app_mask_Detect():
+   
+    MODEL_URL_face = "https://github.com/rajratnapatil9/Face-Mask-With-Django-Website/blob/main/face_detector/res10_300x300_ssd_iter_140000.caffemodel"  # noqa: E501
+    MODEL_face_LOCAL_PATH = HERE / "./face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+
+    MODEL_URL_mask = "https://github.com/rajratnapatil9/Face-Mask-With-Django-Website/blob/main/face_detector/mask_detector.model.h5"  # noqa: E501
+    MODEL_mask_LOCAL_PATH = HERE / "./face_detector/mask_detector.model.h5"
+
+    PROTOTXT_URL_mask = "https://github.com/rajratnapatil9/Face-Mask-With-Django-Website/blob/main/face_detector/deploy.prototxt"  # noqa: E501
+    PROTOTXT_mask_LOCAL_PATH = HERE / "./face_detector/deploy.prototxt"
+
+    download_file(MODEL_URL_face, MODEL_face_LOCAL_PATH, expected_size=23147564)
+    download_file(MODEL_URL_mask, MODEL_face_LOCAL_PATH, expected_size=23147564)
+    download_file(PROTOTXT_URL_mask, PROTOTXT_mask_LOCAL_PATH, expected_size=29353)
+
+    
+
+    class Detection(NamedTuple):
+        name: str
+        prob: float
+
+    
+    class MaskDetect(object):
+
+         def __init__(self):
+             self.vs = VideoStream(src=0).start()
+
+         def __del__(self):
+            cv2.destroyAllWindows()
+
+         def detect_and_predict_mask(self, frame, faceNet, maskNet):
+            # grab the dimensions of the frame and then construct a blob
+            # from it
+            (h, w) = frame.shape[:2]
+            blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                         (104.0, 177.0, 123.0))
+
+            # pass the blob through the network and obtain the face detections
+            faceNet.setInput(blob)
+            detections = faceNet.forward()
+
+            # initialize our list of faces, their corresponding locations,
+            # and the list of predictions from our face mask network
+            faces = []
+            locs = []
+            preds = []
+
+        # loop over the detections
+            for i in range(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with
+                # the detection
+                confidence = detections[0, 0, i, 2]
+
+                # filter out weak detections by ensuring the confidence is
+                # greater than the minimum confidence
+                if confidence > 0.5:
+                    # compute the (x, y)-coordinates of the bounding box for
+                    # the object
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+
+                    # ensure the bounding boxes fall within the dimensions of
+                    # the frame
+                    (startX, startY) = (max(0, startX), max(0, startY))
+                    (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+                    # extract the face ROI, convert it from BGR to RGB channel
+                    # ordering, resize it to 224x224, and preprocess it
+                    face = frame[startY:endY, startX:endX]
+                    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                    face = cv2.resize(face, (224, 224))
+                    face = img_to_array(face)
+                    face = preprocess_input(face)
+
+                    # add the face and bounding boxes to their respective
+                    # lists
+                    faces.append(face)
+                    locs.append((startX, startY, endX, endY))
+
+            # only make a predictions if at least one face was detected
+            if len(faces) > 0:
+                # for faster inference we'll make batch predictions on *all*
+                # faces at the same time rather than one-by-one predictions
+                # in the above `for` loop
+                faces = np.array(faces, dtype="float32")
+                preds = maskNet.predict(faces, batch_size=32)
+
+            # return a 2-tuple of the face locations and their corresponding
+            # locations
+            return (locs, preds)
+
+         def get_frame(self):
+             frame = self.vs.read()
+             frame = imutils.resize(frame, width=650)
+             frame = cv2.flip(frame, 1)
+             # detect faces in the frame and determine if they are wearing a
+              #face mask or not
+             (locs, preds) = self.detect_and_predict_mask(frame, faceNet, maskNet)
+
+            # loop over the detected face locations and their corresponding
+            # locations
+             for (box, pred) in zip(locs, preds):
+                # unpack the bounding box and predictions
+                (startX, startY, endX, endY) = box
+                (mask, withoutMask) = pred
+
+                # determine the class label and color we'll use to draw
+                # the bounding box and text
+                label = "Mask" if mask > withoutMask else "No Mask"
+                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+                # include the probability in the label
+                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+                # display the label and bounding box rectangle on the output
+                # frame
+                cv2.putText(frame, label, (startX, startY - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+             ret, jpeg = cv2.imencode('.jpg', frame)
+             return jpeg.tobytes()
+
+
+    webrtc_ctx = webrtc_streamer(
+        key="mask-detection",
+        mode=WebRtcMode.SENDRECV,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+        video_transformer_factory=MaskDetect,
+        async_transform=False,
+    )
+
+    
 
 def app_video_filters():
     """ Video transforms with OpenCV """
