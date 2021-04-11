95dbdfc1f6dd7e8259309f771b7586fe08d656f9
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-11 04:31:00 -0400

    try13

--- app.py
+++ app.py
@@ -178,7 +178,28 @@ def app_mask_Detect():
    # download_file(PROTOTXT_URL_mask, PROTOTXT_mask_LOCAL_PATH, expected_size=29353)
     
     
+    class VideoCamera(object):
+        def __init__(self):
+            self.video = cv2.VideoCapture(0)
 
+        def __del__(self):
+            self.video.release()
+
+        def get_frame(self):
+            success, image = self.video.read()
+            # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+            # so we must encode it into JPEG in order to correctly display the
+            # video stream.
+
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces_detected = face_detection_videocam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+            for (x, y, w, h) in faces_detected:
+                cv2.rectangle(image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
+            frame_flip = cv2.flip(image, 1)
+            ret, jpeg = cv2.imencode('.jpg', frame_flip)
+            #data = []
+            #data.append(jpeg.tobytes())
+            return jpeg.tobytes()
     class Detection(NamedTuple):
         name: str
         prob: float
