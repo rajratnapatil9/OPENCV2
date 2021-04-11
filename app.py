15ed0291f35d8601fedb6f7a3859df9386b4182c
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-11 07:20:38 -0400

    try31

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ def app_mask_Detect():
                     ret, jpeg = cv2.imencode('.jpg', frame)
              return jpeg.tobytes()
         
-         def transform(self):
+         def transform(self,frame):
              frame = self.vs.read()
              frame = imutils.resize(frame, width=650)
              frame = cv2.flip(frame, 1)
